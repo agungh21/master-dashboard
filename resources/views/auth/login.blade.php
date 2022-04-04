ead4de157a322c8ce7757e2e445e2788da31fc83
agung <muhammadagunghartono@gmail.com>
2022-04-04 13:51:34 +0700

    create template login register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,113 @@
-@extends('layouts.app')
+@extends('layouts.layout_login')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+    <div class="container">
+        <div class="forms-container">
+            <div class="signin-signup">
+                <!-- form login -->
+                <form method="POST" action="{{ route('login') }}" class=" sign-in-form">
+                    @csrf
+                    <h2 class="title">Login</h2>
+                    <div class="input-field">
+                        <div class="icon">
+                            <i class="fas fa-user"></i>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <input type="email" name="email" id="email" placeholder="Email">
+                    </div>
+                    <div class="input-field">
+                        <div class="icon">
+                            <i class="fas fa-lock"></i>
                         </div>
+                        <input type="password" name="password" id="password" placeholder="Password">
+                    </div>
+                    <input type="submit" value="Login" class="btn solid">
+                    {{-- <p class="social-text">or Sign in with social platform</p>
+                    <div class="social-media">
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-google"></i>
+                        </a>
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-linkedin-in"></i>
+                        </a>
+                    </div> --}}
+                </form>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
+                <!-- form register -->
+                <form method="POST" action="{{ route('register') }}" class="sign-up-form">
+                    @csrf
+                    <h2 class="title">Register</h2>
+                    <div class="input-field">
+                        <div class="icon">
+                            <i class="fas fa-user"></i>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
+                        <input type="text" name="name" id="name" placeholder="Nama">
+                    </div>
+                    <div class="input-field">
+                        <div class="icon">
+                            <i class="fas fa-envelope"></i>
                         </div>
-                    </form>
+                        <input type="email" name="email" id="email" placeholder="Email">
+                    </div>
+                    <div class="input-field">
+                        <div class="icon">
+                            <i class="fas fa-lock"></i>
+                        </div>
+                        <input type="password" name="password" id="password" placeholder="Password">
+                    </div>
+                    <div class="input-field">
+                        <div class="icon">
+                            <i class="fas fa-lock"></i>
+                        </div>
+                        <input id="password-confirm" type="password" name="password_confirmation" required
+                            autocomplete="new-password" placeholder="Confirm Password">
+                    </div>
+                    <input type="submit" value="Login" class="btn solid">
+                    {{-- <p class="social-text">or Sign in with social platform</p> --}}
+                    {{-- <div class="social-media">
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-google"></i>
+                        </a>
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-linkedin-in"></i>
+                        </a>
+                    </div> --}}
+                </form>
+            </div>
+        </div>
+
+        <div class="panels-container">
+            <!-- left panel -->
+            <div class="panel left-panel">
+                <div class="content">
+                    <h3>Your here?</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt minus doloremque</p>
+                    <button class="btn transparent" id="sign-up-btn">Register</button>
                 </div>
+
+                <img class="image" src="assets/img/log.svg" alt="">
+            </div>
+            <!-- right panel -->
+            <div class="panel right-panel">
+                <div class="content">
+                    <h3>Your of us ?</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt minus doloremque</p>
+                    <button class="btn transparent" id="sign-in-btn">Daftar</button>
+                </div>
+
+                <img class="image" src="assets/img/register.svg" alt="">
             </div>
         </div>
     </div>
-</div>
 @endsection
