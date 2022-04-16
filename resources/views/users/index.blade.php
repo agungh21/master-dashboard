5a75979957b905736640794cf2bde9dc155fefc5
muhammad_agung_hartono <muhammadagunghartono@gmail.com>
2022-04-16 13:07:01 +0700

    add dashboard role user

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,12 +1,5 @@
-<h1>HELLO USER</h1>
-<a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();"
-    class="
-                         nav-link">
-    <i class="nav-icon fas fa-sign-out-alt"></i>
-    <p>
-        Logout
-    </p>
-</a>
-<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-    @csrf
-</form>
+@extends('layouts.users.back')
+
+@section('content-back')
+    <p>HELLO</p>
+@endsection
